a6de47e6e1d0b26fb82922945f9882eff26fc361
cokkiy <cokkiy@outlook.com>
2024-09-30 16:06:11 +0800

    refine GnssEpochData struct to represent an epoch observation data which includes many sv data

--- lib/src/gnss_epoch_data.rs
+++ lib/src/gnss_epoch_data.rs
@@ -1,6 +1,4 @@
-use gnss_rs::sv::SV;
-use hifitime::Epoch;
-use ssc::SignalStrengthComparer;
+use hifitime::{Duration, Epoch};
 
 use crate::GnssData;
 
@@ -10,10 +8,8 @@ use crate::GnssData;
 pub struct GnssEpochData {
     /// The epoch of the GNSS data.
     pub epoch: Epoch,
-    /// The satellite vehicle (SV).
-    pub sv: SV,
     /// The GNSS data in the epoch.
-    pub data: GnssData,
+    pub data: Vec<GnssData>,
 }
 
 #[allow(dead_code)]
@@ -23,24 +19,18 @@ impl GnssEpochData {
     /// # Arguments
     ///
     /// * `epoch` - The epoch of the GNSS data.
-    /// * `sv` - The satellite vehicle (SV).
     /// * `data` - The GNSS data in the epoch.
     ///
     /// # Returns
     ///
     /// A new `GnssEpochData` instance.
-    pub fn new(epoch: Epoch, sv: SV, data: GnssData) -> Self {
-        Self { epoch, sv, data }
+    pub fn new(epoch: Epoch, data: Vec<GnssData>) -> Self {
+        Self { epoch, data }
     }
 
     /// Retrieves the epoch of the GNSS data.
     pub fn get_epoch(&self) -> Epoch {
         self.epoch
-    }
-
-    /// Retrieves the satellite vehicle (SV).
-    pub fn get_sv(&self) -> SV {
-        self.sv
     }
 
     /// Retrieves the GNSS data in the epoch.
@@ -52,13 +42,21 @@ impl GnssEpochData {
     /// # Note
     ///
     /// This method returns a clone of the GNSS data in the epoch.
-    pub fn get_data(&self) -> GnssData {
+    pub fn get_data(&self) -> Vec<GnssData> {
         self.data.clone()
     }
-}
 
-impl SignalStrengthComparer for GnssEpochData {
-    fn ss_compare(&self, other: &Self) -> Vec<f64> {
-        self.data.ss_compare(&other.data)
+    /// Retrieves the time gap between the current epoch and the other epoch.
+    pub fn time_gap(&self, other: &GnssEpochData) -> Duration {
+        self.epoch - other.epoch
+    }
+
+    /// Iterates over the GNSS data in the epoch.
+    /// # Returns
+    /// An iterator over the GNSS data in the epoch.
+    /// # Note
+    /// This method returns a reference to the GNSS data in the epoch.
+    pub fn next(&self) -> impl Iterator<Item = &GnssData> + '_ {
+        self.data.iter()
     }
 }
