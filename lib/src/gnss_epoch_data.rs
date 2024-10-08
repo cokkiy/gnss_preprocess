789d7edb5d96c796ab9a6b073fbdfdadc67117b9
cokkiy <cokkiy@outlook.com>
2024-10-08 23:42:02 +0800

    modify GnssEpochData definition, change GnssData to SVData make it contains sv prn and add compare method

--- lib/src/gnss_epoch_data.rs
+++ lib/src/gnss_epoch_data.rs
@@ -1,8 +1,10 @@
+use crate::SVData;
+use core::f64;
 use hifitime::{Duration, Epoch};
-
-use crate::GnssData;
+use ssc::SignalStrengthComparer;
 
 /// A struct that represents the station coordinates.
+#[allow(dead_code)]
 #[derive(Clone, Copy, Debug)]
 pub struct Station(f64, f64, f64);
 
@@ -13,13 +15,16 @@ pub struct GnssEpochData {
     /// The epoch of the GNSS data.
     epoch: Epoch,
     /// The GNSS data in the epoch.
-    data: Vec<GnssData>,
+    data: Vec<SVData>,
     /// The station coordinates.
     station: Station,
 }
 
 #[allow(dead_code)]
 impl GnssEpochData {
+    // TODO: maybe we get this max value more reliable
+    // max number of signal strength fields in all type of GNSS data
+    const MAX_SS_FIELDS_NUMBERS: usize = 16;
     /// Creates a new `GnssEpochData` instance.
     ///
     /// # Arguments
@@ -31,7 +36,7 @@ impl GnssEpochData {
     /// # Returns
     ///
     /// A new `GnssEpochData` instance.
-    pub fn new(epoch: Epoch, station: Station, data: Vec<GnssData>) -> Self {
+    pub fn new(epoch: Epoch, station: Station, data: Vec<SVData>) -> Self {
         Self {
             epoch,
             data,
@@ -44,17 +49,13 @@ impl GnssEpochData {
         self.epoch
     }
 
-    /// Retrieves the GNSS data in the epoch.
+    /// Retrieves the SV data in the epoch.
     ///
     /// # Returns
     ///
-    /// The GNSS data in the epoch.
-    ///
-    /// # Note
-    ///
-    /// This method returns a clone of the GNSS data in the epoch.
-    pub fn get_data(&self) -> Vec<GnssData> {
-        self.data.clone()
+    /// The SV data in the epoch.
+    pub fn get_data(&self) -> &Vec<SVData> {
+        self.data.as_ref()
     }
 
     /// Retrieves the station coordinates.
@@ -69,12 +70,33 @@ impl GnssEpochData {
         self.epoch - other.epoch
     }
 
-    /// Iterates over the GNSS data in the epoch.
+    /// Iterates over the SV data in the epoch.
     /// # Returns
-    /// An iterator over the GNSS data in the epoch.
+    /// An iterator over the SV data in the epoch.
     /// # Note
-    /// This method returns a reference to the GNSS data in the epoch.
-    pub fn iter(&self) -> impl Iterator<Item = &GnssData> + '_ {
+    /// This method returns a reference to the SV data in the epoch.
+    pub fn iter(&self) -> impl Iterator<Item = &SVData> + '_ {
         self.data.iter()
+    }
+
+    pub fn signal_strength_compare(&self, other: &GnssEpochData) -> Vec<Vec<f64>> {
+        let mut result = Vec::new();
+        for data in self.iter() {
+            let sv_data = data.get_data();
+            let sv = data.get_sv();
+            let sv_data_other = other
+                .iter()
+                .find(|d| d.get_sv() == sv)
+                .map(|d| d.get_data());
+            if let Some(sv_data_other) = sv_data_other {
+                let mut c_result = sv_data.ss_compare(sv_data_other);
+                //TODO: change 0 to proper value
+                c_result.extend_from_slice(&[0.0; GnssEpochData::MAX_SS_FIELDS_NUMBERS - 0]);
+                result.push(c_result);
+            } else {
+                result.push(vec![f64::MAX; GnssEpochData::MAX_SS_FIELDS_NUMBERS]);
+            }
+        }
+        result
     }
 }
